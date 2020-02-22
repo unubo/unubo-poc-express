f6d30da92ade27bfe35b696aa53822d687071e7e
Contential <hello@contential.io>
2020-02-22 19:20:01 +0000

    Added code

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import config from './src/config';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json(config);
+});
+
+app.listen(PORT, () => {
+  console.log('> unubo-poc - 0.0.1-alpha');
+  console.log(`> Ready on http://localhost:${PORT}...`);
+});
---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+const config = {
+  name: 'unubo-poc-2',
+  version: '0.0.1-alpha',
+};
+
+export default config;
