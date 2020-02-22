4c6c7374a1c526aaeea25ff61e9138b9262fd9d5
Marcel Thomas <marcelthomas5@users.noreply.github.com>
2020-02-22 20:56:27 +0000

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -2,8 +2,8 @@ import _ from 'lodash';
 
 const config = {
   name: 'unubo-3-poc',
-  version: '0.0.1',
-  privateVersion: '0.0.1-alpha',
+  version: '0.0.2',
+  privateVersion: '0.0.2-alpha',
 };
 
 export default _.omit(config, ['privateVersion']);
